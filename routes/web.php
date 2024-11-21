6f089c106b0b574a070b49e0e16bccfa09202165
Rayhan Rizal <rayhanrizal29@gmail.com>
2024-11-21 10:03:34 +0700

    Controller

--- routes/web.php
+++ routes/web.php
@@ -34,39 +34,39 @@ Route::get('/perkalian/{angka}', function ($angka) {
     return 'hasil'. $angka *2;
 });
 
-// // CONTROLLER buku cont
-// Route::get('/bookcont', [BookController::class, 'index'] );
+// CONTROLLER buku cont
+Route::get('/bookcont', [BookController::class, 'index'] );
 
-// // CONTROLLER WITH PARAMETER
-// Route::get('/bukuparameter/{nobuku}', [BookController::class,'show'] );
+// CONTROLLER WITH PARAMETER
+Route::get('/bukuparameter/{nobuku}', [BookController::class,'show'] );
 
-// // REQUEST PENJUMLAHAN ANTAR 2 Parameter
-// Route::get('/tambah',function(Request $request) {
-//     $validate = $request->validate([
-//         'angka1'=> 'required|numeric',
-//         'angka2'=> 'required|numeric',
+// REQUEST PENJUMLAHAN ANTAR 2 Parameter
+Route::get('/tambah',function(Request $request) {
+    $validate = $request->validate([
+        'angka1'=> 'required|numeric',
+        'angka2'=> 'required|numeric',
         
-//     ]);
+    ]);
     
-//     $angka1 = $request->query('angka1',0);
-//     $angka2 = $request->query('angka2',0);
+    $angka1 = $request->query('angka1',0);
+    $angka2 = $request->query('angka2',0);
 
-//     $hasil  = $angka1 + $angka2;
+    $hasil  = $angka1 + $angka2;
 
-//     return view('tambah',compact('angka1','angka2','hasil'));   
-// });
+    return view('tambah',compact('angka1','angka2','hasil'));   
+});
 
-// // RESPONS SIMPLE
-// Route::get('respontes',function(){
-//     return 'hello';
-// });
+// RESPONS SIMPLE
+Route::get('respontes',function(){
+    return 'hello';
+});
 
-// // REDIRECT RESPONS
-// Route::get('/dashboard',function(){
-//     return redirect('home/dashboard');
-// });
+// REDIRECT RESPONS
+Route::get('/dashboard',function(){
+    return redirect('home/dashboard');
+});
 
-// // VIEWS
-// Route::get('/hello', function(){
-//     return view('hello',['name'=>'Rizal']);
-// });
+// VIEWS
+Route::get('/hello', function(){
+    return view('hello',['name'=>'Rizal']);
+});
